0db49efa7ad362ebd209181f71dd3d071f99773a
darika dzh <dzhumalieva.darika@gmail.com>
2017-10-28 12:24:29 +0600

    model User, Devise and ActiveAdmin
    create model User
    add devise to users
    add ActiveAdmin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def access_denied(exception)
+    flash[:danger] = exception.message
+    redirect_to root_url
+  end
+
+  
+  private
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.for(:account_update, keys: [:name])
+  end
+
 end
