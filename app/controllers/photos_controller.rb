f62e64eb605cd5fa10f1b244162f897c3a4c8e55
darika dzh <dzhumalieva.darika@gmail.com>
2017-10-28 16:33:32 +0600

    seeds and view index root page
    add seeds
    fix view index for root page

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,13 +3,13 @@ class PhotosController < ApplicationController
 		@photos = Photo.all
   end
   def new
-  	@photo =  current_user.photos.new(photo_params)
+  	@photo =  current_user.photos.new
   end
 
   def create
   	@photo =  current_user.photos.new(photo_params)
   	if @photo.save
-        redirect_to root_path, :flash => { :notice => "Notice successfully added" }
+        redirect_to root_path, :flash => { :notice => "Photo successfully added" }
       else
       render 'new'
       end
@@ -17,6 +17,11 @@ class PhotosController < ApplicationController
 
   def show
   	@photo = Photo.find(params[:id])
+  end
+
+  def destroy
+    @photo.destroy
+      redirect_to root_path, :flash => { :notice => "Photo successfully deleted" }
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+admin_user = User.create!(name:"Dari",email:"tata@gmail.com",password:"123123",password_confirmation:"123123",admin:"true")
+second_user = User.create!(name:"Murat",email:"renja@gmail.com",password:"123123",password_confirmation:"123123",admin:"false")
+third_user = User.create!(name:"Chimmy",email:"shook@gmail.com",password:"123123",password_confirmation:"123123",admin:"false")
+
+3.times do
+	first_image = Photo.create(name:"Mio", user:admin_user)
+	first_image.avatar = Rails.root.join("app/assets/images/fixtures/mio3.jpeg").open
+	first_image.save!
+end
+3.times do
+	second_image = Photo.create(name:"MoreMio", user:second_user)
+	second_image.avatar = Rails.root.join("app/assets/images/fixtures/mio2.png").open
+	second_image.save!
+end
+3.times do
+	third_image = Photo.create(name:"MioMio", user:third_user)
+	third_image.avatar = Rails.root.join("app/assets/images/fixtures/mio.jpg").open
+	third_image.save!
+end
